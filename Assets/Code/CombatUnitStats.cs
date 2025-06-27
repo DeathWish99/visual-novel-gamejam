1285896495b46c65102e821150ef974f4c4cd42b
Phobez <36761666+Phobez@users.noreply.github.com>
2025-06-27 12:01:54 +0700

    Refactor Combat Unit Stats

--- Assets/Code/CombatUnitStats.cs
+++ Assets/Code/CombatUnitStats.cs
@@ -1,22 +1,33 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu(fileName = "CombatUnitStats", menuName = "Scriptable Objects/CombatUnitStats")]
 [System.Serializable]
 public class CombatUnitStats : ScriptableObject
 {
+    [Header("Identity")]
     [SerializeField] private string unitName;
+    [SerializeField] private Image icon;
+
+    [Header("Allegiance")]
     [SerializeField] private bool isPlayer;
     [SerializeField] private bool isCompanion;
+
+    [Header("Combat Stats")]
     [SerializeField] private int maxHP;
     [SerializeField] private int attack;
     [SerializeField] private int defence;
     [SerializeField] private int speed;
 
+    // Public Properties
     public string UnitName => unitName;
+    public Image Icon => icon;
+
+    public bool IsPlayer => isPlayer;
+    public bool IsCompanion => isCompanion;
+
     public int MaxHP => maxHP;
     public int Attack => attack;
     public int Defence => defence;
     public int Speed => speed;
-    public bool IsPlayer => isPlayer;
-    public bool IsCompanion => isCompanion;
 }
